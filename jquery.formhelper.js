7858b2ecac340c7d1996f3e1e5b43d16808961cf
caphun <caphun@pazu2k.local>
2010-07-21 02:30:31 +0800

    code refactor. added select element.

--- jquery.formhelper.js
+++ jquery.formhelper.js
@@ -11,47 +11,104 @@
  */
  
 (function($){
-	var regex1 = /function\s([^\()]+)/i;
-	
-	$.f =  {
-		text_field: function(options) {
-			if ($.isArray(options) && options.length == 2) {
-				options = {'name': regex1.exec(options[0])[1] + '['+ options[1] +']' }
-			}
-			
-			options = $.extend({
-				id: this._sanitizeId(options['name']),
-				type: 'text'
-			}, options);
-			
-			return  $('<input>').attr(options);
-		},
-		hidden_field: function(options) {
-			return this.text_field($.extend({ type: 'hidden' }, options));
-		},
-		password_field: function(options) {
-			return this.text_field($.extend({ type: 'password' }, options));
-		},
-		check_box: function(options) {
-			return this.text_field($.extend({ type: 'checkbox' }, options));
-		},
-		radio_button: function(options) {
-			return this.text_field($.extend({ type: 'radio' }, options));
-		},
-		label: function(options) {
-			return $('<label></label>').html(options.name);
-		},
-		button: function(options) {
-			return $('<button></button>').attr(options).html('Button');
-		},
-		text_area: function(options) {
-			return $('<textarea></textarea>').attr(options);
-		},
-		_sanitizeId: function(string) {
-			return string.replace(/\[|\]/g, function(match) {
-				return {'[': '_', ']': ''}[match];
-			});
-		}
-	}
-	
+
+var regex1 = /function\s([^\()]+)/i;
+
+$.formhelper = function( options ) {
+    this.options = $.extend( true, {}, $.formhelper.defaults, options );
+    this.init();
+}
+
+$.extend( $.formhelper, {
+
+    defaults: {
+        wrap: '',
+        before_elem: '',
+        after_elem: ''
+    },
+
+    prototype: {
+
+        init: function( ) {},
+
+        // field types
+        text: function( options ) {
+            options = $.extend( { type: 'text' }, this.process_options( options ) );
+            return this.controls['input']( options );
+        },
+
+        hidden: function( options ) {
+            options = $.extend( { type: 'hidden' }, this.process_options( options ) );
+            return this.controls['input']( options );
+        },
+
+        password: function( options ) {
+            options = $.extend( { type: 'password' }, this.process_options( options ) );
+            return this.controls['input']( options );
+        },
+
+        checkbox: function( options ) {
+            options = $.extend( { type: 'checkbox' }, this.process_options( options ) );
+            return this.controls['input']( options );
+        },
+
+        radio: function( options ) {
+            options = $.extend( { type: 'radio' }, this.process_options( options ) );
+            return this.controls['input']( options );
+        },
+        
+        textarea: function( options ) {
+            options = this.process_options( options );
+            return this.controls['textarea']( options );
+        },
+
+        label: function( options ) {
+            options = this.process_options( options );
+            return this.controls['label']( options );
+        },
+        
+        button: function( options ) {
+            return this.controls['button']( options, 'Button' );
+        },
+
+        process_options: function( options ) {
+            if ( $.isArray( options ) && options.length == 2 ) {
+                options = { 'name': regex1.exec(options[0])[1] + '['+ options[1] +']' }
+            }
+            return $.extend({
+                id: this.machine_id( options['name'] || 'input_field' )
+            }, options);
+        },
+
+        machine_id: function( string ) {
+            return string.replace(/\[|\]/g, function( match ) {
+                return {'[': '_', ']': ''}[match];
+            });
+        },
+
+        controls: {
+            label: function( attrs, html ) {
+                return $('<label></label>').attr( attrs ).html( html || attrs['name'] );
+            },
+
+            button: function( attrs, html ) {
+                return $('<button></button>').attr( attrs ).html( html || attrs['value'] );
+            },
+
+            textarea: function( attrs, html ) {
+                return $('<textarea></textarea>').attr( attrs ).text( html || attrs['value'] );
+            },
+
+            input: function( attrs ) {
+                return $('<input />').attr( attrs );
+            },
+            
+            select: function( attrs, list ) {
+                return $('<select></select>').attr( attrs ).append( list );
+            }
+        }
+
+    }
+});
+
 })(jQuery);
